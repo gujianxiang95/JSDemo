9b2d47c5a1501e8be025a6b68657c66184cf5cfa
顾建祥 <gu_jian_xiang@126.com>
2020-09-22 18:01:20 +0800

    get新技巧 for循环里可以更改i值操纵遍历

--- 字符串过滤/index.js
+++ 字符串过滤/index.js
@@ -8,18 +8,18 @@ function handleStr (str) {
     let endIndex =null
     for( let i = 0; i < str.length; i++ ){
         let char = str.charAt(i)
-        if( beginIndex || beginIndex === 0  ){
-            if( i<=endIndex  && i>=beginIndex ){
-                continue
-            }
-        }
+        // if( beginIndex || beginIndex === 0  ){ //如果输入了其实位置，且循环到了起始位置之间 跳过本次循环
+        //     if( i<=endIndex  && i>=beginIndex ){  //可以更改i 不需要你啦
+        //         continue
+        //     }
+        // }
         if( char === '(' ){      //是否遇到左括号
             beginIndex = i
             endIndex = beginIndex + 1   //也许空括号+1检查
             let leftpartTimes = 1      //左括号数量
             let rightpartTimes = 0      //右括号数量
-            while( leftpartTimes !== rightpartTimes ){
-                if( str.charAt(endIndex) === '(' ){    //左括号
+            while( leftpartTimes !== rightpartTimes ){  //左右括号数量相等=>确定括号位置匹配成功
+                if( str.charAt(endIndex) === '(' ){   
                     leftpartTimes++
                 }
                 if( str.charAt(endIndex) === ')' ){
@@ -27,15 +27,14 @@ function handleStr (str) {
                 }
                 endIndex++
             }
-            endIndex--
+            endIndex-- // 减去多加的1
             let index1= beginIndex+1
             let index2= endIndex
             let params = str.substring(index1, index2)
-            
-            let repeatStr = handleStr(params)
+            let repeatStr = handleStr(params)  
             let repactTimes = parseInt(str.charAt(index2+2)) 
             val += repeatStr.repeat( repactTimes )
-            i = endIndex + 3 
+            i = endIndex + 3 //跳过不需要遍历的字符串遍历
         }else{
             val +=char
         }
